d8972dafafb8dd12cf2a3e877327fe94489a8f9f
Jeferson Rodrigues <jeferson_3638@hotmail.com>
2026-07-01 23:39:07 -0300

    chore: configura data-source dinâmico para ambiente de produção e desenvolvimento

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,20 @@
 import { DataSource } from "typeorm";
 import { Usuario } from "./entity/Usuario";
 import { Categoria } from "./entity/Categoria";
-import { Transacao } from "./entity/Transacao"; // O Alves criará esta
+import { Transacao } from "./entity/Transacao"; 
 import { Tag } from "./entity/Tag";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "1012", 
-    database: "gestao_financeira",
+    url: process.env.DATABASE_URL,
+    host: process.env.DATABASE_URL ? undefined : "localhost",
+    port: process.env.DATABASE_URL ? undefined : 5432,
+    username: process.env.DATABASE_URL ? undefined : "postgres",
+    password: process.env.DATABASE_URL ? undefined : "1012", 
+    database: process.env.DATABASE_URL ? undefined : "gestao_financeira",
+    
     entities: [Usuario, Categoria, Transacao, Tag],
     synchronize: true, 
-    logging: true,
+    logging: process.env.DATABASE_URL ? false : true,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
