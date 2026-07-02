704bd84e5186b90461ebed9653c6f8d4d0e7b600
Jeferson Rodrigues <jeferson_3638@hotmail.com>
2026-07-01 23:13:11 -0300

    fix: ajusta propriedade de mensagem do enum zod no categoria-schema

--- src/schemas/categoria-schema.ts
+++ src/schemas/categoria-schema.ts
@@ -2,5 +2,7 @@ import { z } from "zod";
 
 export const categoriaSchema = z.object({
     nome: z.string().min(2, "O nome da categoria é obrigatório"),
-    tipo: z.enum(["entrada", "saida"], { required_error: "Tipo deve ser 'entrada' ou 'saida'" })
+    tipo: z.enum(["entrada", "saida"], { 
+        message: "Tipo deve ser 'entrada' ou 'saida'" 
+    })
 });
